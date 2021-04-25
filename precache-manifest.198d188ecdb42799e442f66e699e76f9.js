b4abf58b1a0bbb945bb31d276d7f1ae929f56c8c
Matthew <mgp271@gmail.com>
2021-04-25 14:19:39 -0700

    api fix

---
+++ precache-manifest.198d188ecdb42799e442f66e699e76f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb2e940b6b745a9eae31e67c6436898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f78a236d5557cdac3741",
+    "url": "/static/css/2.787a0b37.chunk.css"
+  },
+  {
+    "revision": "9465534c9b554656ba75",
+    "url": "/static/css/main.b14285f6.chunk.css"
+  },
+  {
+    "revision": "f78a236d5557cdac3741",
+    "url": "/static/js/2.1257f849.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1257f849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9465534c9b554656ba75",
+    "url": "/static/js/main.ac09c37c.chunk.js"
+  },
+  {
+    "revision": "d34bd062055391db11d3",
+    "url": "/static/js/runtime-main.3127bc2d.js"
+  },
+  {
+    "revision": "d9675d3c318502397fd773ed22c9f237",
+    "url": "/static/media/fortnite.d9675d3c.jpg"
+  },
+  {
+    "revision": "651f78dd44ca82db756f2b84381c9629",
+    "url": "/static/media/ghostrunner.651f78dd.jpg"
+  },
+  {
+    "revision": "604f0a8c70e412a4bc53589391478a1d",
+    "url": "/static/media/logo-mobile.604f0a8c.svg"
+  },
+  {
+    "revision": "6217c48ca6d2a8ddf9a0836f0fd0697b",
+    "url": "/static/media/logo.6217c48c.svg"
+  }
+]);
